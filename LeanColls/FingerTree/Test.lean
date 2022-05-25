63776100cbca36b6f0bd88a8d3bfdaba87fc001d
James <jamesgallicchio@gmail.com>
2022-05-24 23:29:40 -0400

    Refactor finger tree tests

--- LeanColls/FingerTree/Test.lean
+++ LeanColls/FingerTree/Test.lean
@@ -5,75 +5,65 @@ Authors: James Gallicchio
 -/
 
 import LeanColls.AuxLemmas
+import LeanColls.Classes
 import LeanColls.FingerTree.FingerTree
+import LeanColls.FingerTree.RadixBalancedFT
+import LeanColls.TestHelpers
 
-def ephReps := 5000000
-def persReps := 10000
+open LeanColls
 
-def testFT : IO (Nat × Nat) := do
+def ephReps := 500000
+def persReps := 50000
+def altReps := 500000
+
+def testColl (α) [Enumerable α Nat] [Iterable α Nat] : IO (Nat × Nat × Nat) := do
   let (tEph,_) ← time (do
-    let mut q : FingerTree Nat 0 := FingerTree.Empty
+    let mut q := Enumerable.insert none
     for i in [:ephReps] do
-      q := q.snoc i
+      q := Enumerable.insert (some (i, q))
+    let mut q' : Iterable.ρ α Nat := Iterable.toIterator <| Enumerable.fromEnumerator q
     for i in [:ephReps] do
-      match q.back? with
+      match Iterable.step q' with
       | none => IO.print "Early empty"
-      | some (q',(x : Nat)) =>
+      | some (x,q'') =>
         if x = ephReps - 1 - i then
-          q := q'
+          q' := q''
         else
           IO.print "Wrong number"
   )
   let (tPers,_) ← time (do
-    let mut q : FingerTree Nat 0 := FingerTree.Empty
+    let mut q := Enumerable.insert none
     for i in [:persReps] do
-      q := FingerTree.snoc q i
+      q := Enumerable.insert (some (i,q))
+    let mut q' : Iterable.ρ α Nat := Iterable.toIterator <| Enumerable.fromEnumerator q
     for i in [:persReps] do
-      let q1 := q.back?
-      match q1 with
+      match Iterable.step q' with
       | none => IO.print "jinkies"
-      | some (q',(x:Nat)) =>
+      | some (x,q'') =>
         if x = persReps - 1 - i then
-          q := q'
+          q' := q''
         else
           IO.print "jeepers"
   )
-  return (tEph, tPers)
-
-def testArr : IO (Nat × Nat) := do
-  let (tEph,_) ← time (do
-    let mut q : Array Nat := Array.empty
-    for i in [:ephReps] do
-      q := q.push i
-    for i in [:ephReps] do
-      match q.back? with
-      | none => IO.print "Early empty"
-      | some (x : Nat) =>
-        if x = ephReps - 1 - i then
-          q := q.pop
-        else
-          IO.print s!"Got {x} instead of {ephReps-1-i}"
-          break
-  )
-  let (tPers,_) ← time (do
-    let mut q : Array Nat := Array.empty
+  let (tAlt,_) ← time (do
     for i in [:persReps] do
-      q := q.push i
-    for i in [:persReps] do
-      let q1 := q.pop
-      match q.back? with
-      | none => IO.print "jinkies"
-      | some (x:Nat) =>
-        if x = persReps - 1 - i then
-          q := q1
-        else
-          IO.print "jeepers"
+      let q := Enumerable.insert (some (i,Enumerable.insert none))
+      let q' : Iterable.ρ α Nat := Iterable.toIterator <| Enumerable.fromEnumerator q
+      match Iterable.step q' with
+      | none => IO.print "wh!"
+      | some (x,q'') =>
+        if x ≠ i then
+          IO.print "whaa!"
   )
-  return (tEph, tPers)
+  return (tEph, tPers, tAlt)
 
-def testAll : IO Unit := do
-  let (ftEph, ftPers) ← testFT
-  let (arrEph, arrPers) ← testArr
-  IO.print s!"           | Ephemeral\t| Persistent\n"
-  IO.print s!"FingerTree | {ftEph}\t| {ftPers}\n"
-  IO.print s!"Array      | {arrEph}\t| {arrPers}\n"
+def testAll : IO Unit :=
+  open LeanColls in do
+  let toStrLine := λ (eph, pers, alt) => [toString eph, toString pers, toString alt]
+  IO.println <| formatTable
+    ["Name", "Ephemeral", "Persistent", "Alternate"] [
+      "FingerTree" :: toStrLine (← testColl (FingerTree Nat 0)),
+      --"Array" :: toStrLine (← testColl (Array Nat)),
+      "RBFT" :: toStrLine (← testColl (RadixBalancedFT Nat))
+    ]
+    (by sorry)
