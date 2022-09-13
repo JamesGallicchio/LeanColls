26e0a2bd44a0c9954915257ad3b9cd504e545e34
James <jamesgallicchio@gmail.com>
2022-09-13 17:06:09 -0400

    Precompile modules

--- lakefile.lean
+++ lakefile.lean
@@ -2,19 +2,28 @@ import Lake
 open System Lake DSL
 
 package leancolls {
-  precompileModules := false
+  precompileModules := true
 }
 
 @[defaultTarget]
-lean_lib LeanColls {
-  srcDir := __dir__
-  roots := #[`LeanColls]
-}
+lean_lib LeanColls
 
 @[defaultTarget]
 lean_exe test {
   root := `Main
 }
+
+target leancolls_array.o (pkg : Package) : FilePath := do
+  let oFile := pkg.buildDir / "c" / "leancolls_array.o"
+  let srcJob ← inputFile <| pkg.dir / "bindings" / "leancolls_array.c"
+  buildFileAfterDep oFile srcJob fun srcFile => do
+    let flags := #["-I", (← getLeanIncludeDir).toString]
+    compileO "leancolls_array.c" oFile srcFile flags
+
+extern_lib libleancolls_array (pkg : Package) := do
+  let name := nameToStaticLib "leancolls_array"
+  let ffiO ← fetch <| pkg.target ``leancolls_array.o
+  buildStaticLib (pkg.buildDir / "lib" / name) #[ffiO]
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4" @ "master"
