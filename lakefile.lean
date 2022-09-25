e946a05640667b97b0581a61f569b931591f8437
James <jamesgallicchio@gmail.com>
2022-09-25 19:43:53 -0400

    Update doc-gen4 version

--- lakefile.lean
+++ lakefile.lean
@@ -29,4 +29,4 @@ require mathlib from git
   "https://github.com/leanprover-community/mathlib4" @ "master"
 
 meta if get_config? env = some "dev" then -- dev is so not everyone has to build it
-require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
+require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "upgrade-lean"
