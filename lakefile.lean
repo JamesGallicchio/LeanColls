b70e585f7b4927e71826269e0cecf0c5318bb6b4
James <jamesgallicchio@gmail.com>
2024-06-08 04:39:10 -0400

    fix doc-gen dependency

--- lakefile.lean
+++ lakefile.lean
@@ -14,4 +14,6 @@ require mathlib from git
   "https://github.com/leanprover-community/mathlib4" @ "v4.8.0"
 
 meta if get_config? doc = some "on" then
-  require «doc-gen4» from git "https://github.com/leanprover/doc-gen4" @ "main"
+  require «doc-gen4» from git "https://github.com/leanprover/doc-gen4"
+    @ "cf138201a0a4fa8ca78b6e2a42a0a4860369d10e"
+
